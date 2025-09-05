cb2f0f993cf68f944247c6d625e2c3077afe4ae5
hrishi2099 <hrishikeshjagtap05@gmail.com>
2025-09-05 16:02:32 +0530

    feat: add placeholder logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,8 +34,8 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
-            <Link href="/" className="text-2xl font-bold text-black dark:text-white">
-              NutriSap
+            <Link href="/" className="flex items-center">
+              <img src="/logo.svg" alt="NutriSap Logo" className="h-10" />
             </Link>
           </div>
           
